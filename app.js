df7a2ef8ea54cedfadd8e902f8e48cee22efdab0
rafaelmuto <r.nagahama@gmail.com>
2020-04-30 01:28:51 -0300

    setting up for frontend

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 console.log('==> running app.js');
 
 const express = require('express');
+const path = require('path');
 const Datastore = require('nedb');
 const led = require('sense-hat-led');
 const Joystick = require('sense-hat-joystick').Joystick;
@@ -12,6 +13,10 @@ const app = express();
 // starting neDB:
 const db = new Datastore('database.db');
 db.loadDatabase();
+// setting view engine:
+app.set('view engine', 'pug');
+// serving static files folder:
+app.use(express.static(path.join(__dirname, 'public')));
 
 // sense-hat joystick test:
 const joystick = new Joystick();
@@ -39,12 +44,19 @@ joystick.on('right', () => {
 
 // ==> middlewares:
 app.use('/api', apiRouter);
+
 app.use('/', (req, res, nxt) => {
   res.send('hello world from raspberry pi + sensehat');
+});
+
+app.use((req, res, nxt) => {
+  console.log('=> Err404');
+  res.status(404).send('Err404');
 });
 
 app.listen(8080, () => {
   led.sync.clear();
   console.log('-> starting server @ port 8080');
   led.sync.showMessage('raspberry pi nodeJS server on...', 0.1 / 2);
+  led.sync.clear();
 });
