7412e6c2e0d2d89c0a1cb7b95ffd94343e06c41c
rafaelmuto <r.nagahama@gmail.com>
2020-05-22 18:15:23 -0300

    api show text frontend

--- Public/js/main.js
+++ Public/js/main.js
@@ -2,6 +2,7 @@ console.log('main.js');
 
 document.getElementById('api_led_clear_btn').addEventListener('click', () => {
   console.log('api_led_clear_btn clicked!');
+
   fetch('/api/led/clear')
     .then(res => res.json())
     .then(data => console.log(data))
@@ -28,3 +29,16 @@ document.getElementById('api_led_fill_btn').addEventListener('click', () => {
     .then(data => console.log(data))
     .catch(err => console.log(err));
 });
+
+document
+  .getElementById('api_led_text_std_btn')
+  .addEventListener('click', () => {
+    console.log('api_led_text_std_btn clicked!');
+
+    const text = document.getElementById('led_text_std').value;
+
+    fetch('/api/led/showtxt/' + text)
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  });
