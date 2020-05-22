2a4ed8414324fa28438f94070b3e189cf14031c7
rafaelmuto <r.nagahama@gmail.com>
2020-05-22 11:21:03 -0300

    led fill endpoint

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,8 +1,21 @@
 console.log('main.js');
 
 document.getElementById('api_led_clear_btn').addEventListener('click', () => {
-  console.log('api_led_clear clicked!');
+  console.log('api_led_clear_btn clicked!');
   fetch('/api/led/clear')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+});
+
+document.getElementById('api_led_fill_btn').addEventListener('click', () => {
+  console.log('api_led_fill_btn clicked');
+
+  let R = document.getElementById('led_fill_R').value;
+  let G = document.getElementById('led_fill_G').value;
+  let B = document.getElementById('led_fill_B').value;
+
+  fetch('/api/led/fill/' + R + '/' + G + '/' + B)
     .then(res => res.json())
     .then(data => console.log(data))
     .catch(err => console.log(err));
